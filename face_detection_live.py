dc9b88f4763d2c91d29d8453a3657616b31c9c56
Amanullah Aman <amancse.aiub@gmail.com>
2024-04-24 11:51:22 +0600

    detect front face from webcame

---
+++ face_detection_live.py
@@ -0,0 +1,34 @@
+import cv2
+
+# import trained module
+
+cascade_classifier = cv2.CascadeClassifier("data/haarcascade_frontalface_alt2.xml")
+
+# capture video using webcam
+video_capture = cv2.VideoCapture(0)
+
+# setting width and height
+video_capture.set(3, 640)
+video_capture.set(4, 480)
+
+while True:
+    # returns next video frame
+    ret, frame = video_capture.read()
+
+    # transform to gray image
+    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = cascade_classifier.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 5)
+
+    cv2.imshow("Face detect from live camera", frame)
+
+    # exit
+    key = cv2.waitKey(30) & 0xff
+    if key == 27:
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
